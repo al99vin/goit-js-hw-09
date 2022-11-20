89af99e0865ca41c77c764703fa9232532bd4e54
al99vin <al99vin1@gmail.com>
2022-11-20 20:09:03 +0200

    d

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.form')
+const body = document.querySelector('.body')
+const startB = document.querySelector('[data-start]')
+const stopB = document.querySelector('[data-stop]')
+
+stopB.disabled = true;
+
+startB.addEventListener('click', event => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+    stopB.disabled = false;
+    startB.disabled = true;
+});
+stopB.addEventListener('click', event => {
+    clearInterval(timerId);
+    startB.disabled = false;
+    stopB.disabled = true;
+});
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
